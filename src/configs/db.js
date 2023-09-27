e8b1fc10af5f1477872f3eec3d31954635de74b9
Prashanth2856 <prashanthnani2856@gmail.com>
2023-09-27 10:14:18 +0530

    changed env variables

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+require("dotenv").config()
 
 const connect = () => {
   return mongoose.connect(
-    "mongodb+srv://prashanth:prashanth2856@cluster0.fol3d.mongodb.net/skinstore?retryWrites=true&w=majority",
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.fol3d.mongodb.net/skinstore?retryWrites=true&w=majority`,
     {
       useNewUrlParser: true,
       useCreateIndex: true,
